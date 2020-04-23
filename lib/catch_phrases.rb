71677cc9e7f7638a7a2ce5f05303ef983ff54a23
Anastasia Orlova <metkusblackus@gmail.com>
2020-04-23 01:18:06 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,15 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  warning = "It's Dangerous To Go Alone! Take This."
+  puts warning
+end
+
+
+def all_phrases 
+  puts mario
+  puts toadstool
+  puts link 
+end 
